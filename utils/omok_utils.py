cb8b8bbd6f8ebf419dae731c92dfd3ecc8e814ba
WonHyung Shin <wonhyung64@icloud.com>
2022-04-25 19:29:17 +0900

    feat - def description

--- utils/omok_utils.py
+++ utils/omok_utils.py
@@ -1,7 +1,14 @@
-def generate_board():
+def generate_board()-> str:
+    """
+    generate Omok board
+
+    Returns:
+        str: Omok board
+    """
     while True:
         board_size = int(input("Board size (>10) :"))
-        if board_size > 10: break
+        if board_size > 10:
+            break
 
     board_top = f"┌{'┬'*(board_size - 2)}┐"
     board_mid = f"├{'┼'*(board_size - 2)}┤"
